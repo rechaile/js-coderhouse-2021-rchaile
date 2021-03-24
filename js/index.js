70f0ef1c05591690b0e04bccfd2c67d2c8909a0f
Roberto Chaile <rechaile@gmail.com>
2021-03-24 19:39:33 -0300

    condicional de incompatibilidad hipertensión

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-let nombre = prompt('¿Cuál es tu nombre?')
-let apellido = prompt('¿Cuál es tu apellido?')
+const enfermedad = 'hipertensión'
+const medicamento = 'diclofenac'
+
+let inputEnfermedad = prompt('Ingrese la enfermedad crónica que padece')
+let inputMedicamento = prompt('Ingrese un antiinflamatorio que consuma habitualmente')
 const espacio= (' ')
 
-alert('Bienvenido a Medichek '+nombre+espacio+apellido)
+if (inputEnfermedad === enfermedad && inputMedicamento === medicamento) {
+    alert('¡Precaución el '+ medicamento +' está contraindicado para personas con '+ enfermedad +'! Puede causar subas de presión y disminución del efecto de su antihipertensivo. Ante cualquier duda consulte a su médico o farmacéutico')
+} else {
+    alert ('No se han encontrado incompatibilidades')
+}
+
+
