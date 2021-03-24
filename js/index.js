db0f7d52e50bd5ba860491c49ad11f0d9e570e87
Roberto Chaile <rechaile@gmail.com>
2021-03-24 19:41:20 -0300

    borré const espacio

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const medicamento = 'diclofenac'
 
 let inputEnfermedad = prompt('Ingrese la enfermedad crónica que padece')
 let inputMedicamento = prompt('Ingrese un antiinflamatorio que consuma habitualmente')
-const espacio= (' ')
+
 
 if (inputEnfermedad === enfermedad && inputMedicamento === medicamento) {
     alert('¡Precaución el '+ medicamento +' está contraindicado para personas con '+ enfermedad +'! Puede causar subas de presión y disminución del efecto de su antihipertensivo. Ante cualquier duda consulte a su médico o farmacéutico')
