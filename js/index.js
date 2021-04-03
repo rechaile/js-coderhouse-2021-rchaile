099f6518de361a5de25620701e1d505716ce26d0
Roberto Chaile <rechaile@gmail.com>
2021-04-03 18:31:22 -0300

    simulador desafío 2 incompatibilidades

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
-const enfermedad = 'hipertensión'
-const medicamento = 'diclofenac'
 
-let inputEnfermedad = prompt('Ingrese la enfermedad crónica que padece')
-let inputMedicamento = prompt('Ingrese un antiinflamatorio que consuma habitualmente')
+function inputEnfermedad () {
+    let enfermedad = prompt("Ingrese la enfermedad crónica que padece")
+    return enfermedad
+    }
 
+function inputMedicamento () {
+    let medicamento = prompt('Ingrese un medicamento que consuma habitualmente')
+    return medicamento }
 
-if (inputEnfermedad === enfermedad && inputMedicamento === medicamento) {
-    alert('¡Precaución el '+ medicamento +' está contraindicado para personas con '+ enfermedad +'! Puede causar subas de presión y disminución del efecto de su antihipertensivo. Ante cualquier duda consulte a su médico o farmacéutico')
-} else {
-    alert ('No se han encontrado incompatibilidades')
+function incompatibilidades (enfermedad, medicamento) {
+    while (enfermedad && medicamento !== 'esc') {
+
+    switch (enfermedad, medicamento) {
+        case "diabetes" && "betametasona":
+            alert ('¡Cuidado! el consumo de corticoides en personas diabéticas puede causar desequilibrios peligrosos en la glucemia. Esta contrainidicado en pacientes diabéticos')
+            break
+        case "diabetes" && "diclofenac":
+            alert ('Precaución: El consumo de antiinflamatorios de forma crónica no está recomendado en personas diabéticas porque puede perjudicar la función renal')
+            break
+        case "hipertension" && "betametasona":
+            alert ('Precaución: El consumo de corticoides de forma crónica no está recomendado en personas hipertensas porque puede perjudicar la función renal del paciente')
+            break
+        case "hipertension" && "diclofenac":
+            alert ('¡Cuidado! El diclofenac sódico puede contribuir a la suba de presión del paciente hipertenso. Trate de utilizar otras alternativas antiinflamatorias o consulte con su médico para usarlo adecuadamente')
+            break
+        default:
+            alert ('No se han encontrado incompatibilidades')
+        }
+    }
 }
 
+let enfermedad = inputEnfermedad()
+let medicamento = inputMedicamento()
+
+incompatibilidades (enfermedad, medicamento)
 
