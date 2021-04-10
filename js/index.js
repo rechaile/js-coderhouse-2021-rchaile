420deba40b71fcf8d373408c8160f25402a288b3
Roberto Chaile <rechaile@gmail.com>
2021-04-10 13:12:59 -0300

    pusheo cambios con objetos

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-function inputEnfermedad () {
+/*function inputEnfermedad () {
     let enfermedad = prompt("Ingrese la enfermedad crónica que padece")
     return enfermedad
     }
@@ -32,5 +32,54 @@ function incompatibilidades (enfermedad, medicamento) {
 let enfermedad = inputEnfermedad()
 let medicamento = inputMedicamento()
 
-incompatibilidades (enfermedad, medicamento)
+incompatibilidades (enfermedad, medicamento)*/
 
+
+
+const consulta = {
+    
+    enfermedad:     
+    function inputEnfermedad () {
+        let enfermedad = prompt("Ingrese la enfermedad crónica que padece")
+        return enfermedad
+        }
+    
+    medicamento: 
+    
+    function inputMedicamento () {
+        let medicamento = prompt('Ingrese un medicamento que consuma habitualmente')
+        return medicamento }
+}
+
+function incompatibilidades ( consulta.enfermedad, consulta.medicamento ) {
+    
+    switch (consulta.enfermedad, consulta.medicamento) {
+        case "diabetes" && "betametasona":
+            alert ('¡Cuidado! el consumo de corticoides en personas diabéticas puede causar desequilibrios peligrosos en la glucemia. Esta contrainidicado en pacientes diabéticos')
+            break
+        case "diabetes" && "diclofenac":
+            alert ('Precaución: El consumo de antiinflamatorios de forma crónica no está recomendado en personas diabéticas porque puede perjudicar la función renal')
+            break
+        case "hipertension" && "betametasona":
+            alert ('Precaución: El consumo de corticoides de forma crónica no está recomendado en personas hipertensas porque puede perjudicar la función renal del paciente')
+            break
+        case "hipertension" && "diclofenac":
+            alert ('¡Cuidado! El diclofenac sódico puede contribuir a la suba de presión del paciente hipertenso. Trate de utilizar otras alternativas antiinflamatorias o consulte con su médico para usarlo adecuadamente')
+            break
+        default:
+            alert ('No se han encontrado incompatibilidades')
+        }
+    }
+
+    consulta.enfermedad ()
+    consulta.medicamento ()
+    incompatibilidades (consulta.enfermedad, consulta.medicamento)
+    
+function messageAlert () {
+    alert= ('messages')
+}
+
+    messages = {
+        diabetesBetametasona: '¡Cuidado! el consumo de corticoides en personas diabéticas puede causar desequilibrios peligrosos en la glucemia. Esta contrainidicado en pacientes diabéticos'
+    }
+    
