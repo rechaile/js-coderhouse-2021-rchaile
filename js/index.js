868d4a22a3c9fa15a812e8e7e9779edfb1ed3083
Roberto Chaile <rechaile@gmail.com>
2021-04-17 22:07:31 -0300

    commit de estilos, animaciones wow, estructura mas completa del simulador

--- js/index.js
+++ js/index.js
@@ -20,9 +20,15 @@ function inputMedicamento() {
 
 const messages = {
     diabeBeta: "¡Cuidado! el consumo de corticoides en personas diabéticas puede causar desequilibrios peligrosos en la glucemia. Esta contrainidicado en pacientes diabéticos",
+    
     diabeDiclo: "Precaución: El consumo de antiinflamatorios de forma crónica no está recomendado en personas diabéticas porque puede perjudicar la función renal",
+
+    diabFuro: "¡Cuidado! el consumo de diuréticos como la furosemida en personas diabéticas puede causar desequilibrios peligrosos en la hidratación y llevar al colapso. Esta contrainidicado en pacientes diabéticos",
+    
     hipertenBeta: "Precaución: El consumo de corticoides de forma crónica no está recomendado en personas hipertensas porque puede perjudicar la función renal del paciente",
+    
     hipertenDiclo: "¡Cuidado! El diclofenac sódico puede contribuir a la suba de presión del paciente hipertenso. Trate de utilizar otras alternativas antiinflamatorias o consulte con su médico para usarlo adecuadamente",
+
     nullmessage: "No se han encontrado incompatibilidades"
 }
 
@@ -41,9 +47,12 @@ function incompatibilidades(enfermedad, medicamento) {
             } else
                 if (enfermedad === 'hipertension' && medicamento === 'diclofenac') {
                     alert (messages.hipertenDiclo)
-                } else {
-                    alert (messages.nullmessage)
-                }
+                }   else
+                      if (enfermedad === 'diabetes' && medicamento === 'furosemida') {
+                            alert (messages.diabFuro)
+                        }   else {
+                                alert (messages.nullmessage)
+                            }
 }
 
 // Función de Consulta y chequeo de incompatibilidad
