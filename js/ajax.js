74b74b8e6303b96c48b14155fd2d8994516d6ade
Roberto Chaile <rechaile@gmail.com>
2021-05-23 00:50:02 -0300

    se agregó la fn llamarapi y mostrar casos de covid por ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,17 @@
 
+const llamarApi = ()=> {
+$.get('https://covid-api.mmediagroup.fr/v1/cases', function(response) { 
+        console.log(response['Argentina'])
+        $(".covid-counter").text(response['Argentina'].All.confirmed)
+    }) 
+}
 
-$.get(`https://covid-api.mmediagroup.fr/v1/cases?country=Argentina`, 
-    function(response){ 
-        console.log(response)}) 
+const mostrarCovid = ()=> {$(".btnActualizar").on('click', llamarApi(), ()=> {
+    $(".covid-counter").fadeIn(400, "swing")
+})}
+
+mostrarCovid()
+    
    
  
 
