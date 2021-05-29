cd6cbbd7adb2185a48a839d8ddace107ad8fdfff
Roberto Chaile <rechaile@gmail.com>
2021-05-29 20:55:55 -0300

    simulador con strategy y modal estilado completo con scroll

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 
 import {fadeLogo, slideFoto, slideForms, slideParrafo, abrirMenu} from "./efectos.js"
-//import {messages} from "./mensajes.js"
 
 //Animaciones
 fadeLogo()
@@ -9,26 +8,7 @@ slideFoto()
 slideForms()
 abrirMenu()
 
-// Storage y creación de array Consultas
-
-class Consulta {
-    constructor (nombre, enfermedad, medicamento) {
-        this.nombre = nombre,
-        this.enfermedad = enfermedad,
-        this.medicamento = medicamento
-    }
-}
-
-let consultas = JSON.parse (localStorage.getItem('consultas'))
-
-if (!consultas) {
-    consultas = []
-}
-
-const create = (Consulta) => {
-            consultas.push (Consulta)
-        }
-
+//Funciones Modal
 const verModal = () => {
     $("#alert-incompatibilidad").toggleClass('modal-active')
 }
@@ -36,104 +16,81 @@ const verModal = () => {
 const cerrarVentana = () => {
     $("#modal-cerrar").on('click', ()=> {
         $("#alert-incompatibilidad").toggleClass('modal-active')
-    })
-}
-   
- //Valores de inputs
-
- //const nombre = $("#input-nombre").val().toLowerCase()
-       
-let incompatibilidad = []
-
-
-
-const analisisIncompat = async ()=> {
-    let resp = await fetch('./json/incompatibilidades.json')
-    const data = await resp.json()
-    incompatibilidad = data
-
-    const enfermedad = $("#enfermedad").val()
-    const medicamento = $("#medicamento").val()
-    
-    let filtrado = incompatibilidad.filter(el => el.enfermedad === enfermedad && el.medicamento === medicamento)
-    const alerta = ()=> $("#alert-message").text(resultAlerta)
-    const resultAlerta = filtrado.map (el => el.mensajeAlerta)
-    
-    $("#form-incompatibilidad").on('submit', (event) => {
-        
-        event.preventDefault()
-        alerta()
-        verModal()
-        
-        $("#input-nombre").val('Ingresa tu nombre completo') 
-        $("#input-enfermedad").val('ej: diabetes') 
-        $("#input-medicamento").val('ej: diclofenac')
         }
     )
 }
 
+// Funciones Simulador
 
-analisisIncompat()
+let incompatibilidad = []
+let interaccion = []
 
-cerrarVentana()  
-
-
-//Función de Análisis de Incompatibilidad
-
-
-
-   
-
-
-/** function incompatibilidades(enfermedad, medicamento) {
-
-    if (enfermedad === 'diabetes' && medicamento === 'betametasona') {
-        $("#alert-message").text(messages.diabeBeta)
-    } else
-        if (enfermedad === 'diabetes' && medicamento === 'diclofenac') {
-            $("#alert-message").text(messages.diabeDiclo)
-        } else
-            if (enfermedad === 'hipertension' && medicamento === 'betametasona') {
-                $("#alert-message").text(messages.hipertenBeta)
-            } else
-                if (enfermedad === 'hipertension' && medicamento === 'diclofenac') {
-                    $("#alert-message").text(messages.hipertenDiclo)
-                }   else
-                    if (enfermedad === 'diabetes' && medicamento === 'furosemida') {
-                        $("#alert-message").text(messages.diabFuro)
-                        }  else {
-                                $("#alert-message").text(messages.nullmessage)
-                            }
+//Interacción Base de datos
+const traerJSONincompat = async ()=> {
+    let resp = await fetch('./json/incompatibilidades.json')
+    const data = await resp.json()
+    incompatibilidad = data
 }
 
-//Eventos de Formulario Incompatibilidad y analisis de resultados
+const traerJSONinterac = async ()=> {
+    let resp = await fetch('./json/interacciones.json')
+    const data = await resp.json()
+    interaccion = data
+}
 
-$("#form-incompatibilidad").on('submit', (event) => {
-
-    event.preventDefault()
+//Estrategias de análisis
+const stratIncompat = () => { 
     
-    //Valores de inputs
+    $("#form-incompatibilidad").on('submit', (event) => {
+        event.preventDefault()
+        const enfermedad = $("#enfermedad").val().toLowerCase()
+        const medicamento = $("#medicamento").val().toLowerCase()
 
-    //const nombre = $("#input-nombre").val().toLowerCase()
-       
-    const enfermedad = $("#input-enfermedad").val().toLowerCase()
+        let findIncompat = incompatibilidad.filter(el => el.enfermedad === enfermedad && el.medicamento === medicamento)
+        const alerta = ()=> $("#alert-message").text(resultAlerta)
+        const resultAlerta = findIncompat.map (el => el.mensajeAlerta)
+        
+        alerta()
+        verModal()
+        
+        $("#enfermedad").val('ej: diabetes') 
+        $("#medicamento").val('ej: diclofenac')
+        }
+    )
+}
+
+const stratInterac = () => { 
     
-    const medicamento = $("#input-medicamento").val().toLowerCase()
-  
-    //Objeto consulta
-    //const consulta = new Consulta (nombre, enfermedad, medicamento)
-    //create (consulta)
+    $("#form-interaccion").on('submit', (event) => {
+        
+        event.preventDefault()
+        const medicamento1 = $("#medicamento1").val().toLowerCase()
+        const medicamento2 = $("#medicamento2").val().toLowerCase()
 
-    //localStorage.setItem('consultas', JSON.stringify(consultas))
+        let findInterac = interaccion.filter(el => el.med1 === medicamento1 || medicamento2 && el.med2 === medicamento2 || medicamento1)
+        const alerta = ()=> $("#alert-message").text(resultAlerta)
+        const resultAlerta = findInterac.map (el => el.mensajeAlerta)
+        
+        alerta()
+        verModal()
+        
+        $("#medicamento1").val('ej: diclofenac') 
+        $("#medicamento2").val('ej: betametasona')
+        }
+    )
+}
 
-    incompatibilidades (enfermedad, medicamento)
+//Análisis
+const analisisIncompat = ()=> {
+    traerJSONincompat()
+    stratIncompat()
+    }
 
-    verModal()
+const analisisInterac = () => {
+    traerJSONinterac()
+    stratInterac()
+    }
 
-    $("#input-nombre").val('Ingresa tu nombre completo') 
-    $("#input-enfermedad").val('ej: diabetes') 
-    $("#input-medicamento").val('ej: diclofenac')
-
-    })
-
-    cerrarVentana()**/    
+analisisIncompat()
+analisisInterac()
+cerrarVentana()  
